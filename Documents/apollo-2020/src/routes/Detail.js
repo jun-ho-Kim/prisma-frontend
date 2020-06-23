b47c84aac42edfe8f01881da509d6576b151be88
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-06-23 16:02:58 +0900

    Get query Post data

--- Documents/apollo-2020/src/routes/Detail.js
+++ Documents/apollo-2020/src/routes/Detail.js
@@ -1,1 +1,100 @@
-export default () => "Detail";
+import React from "react";
+import { useParams } from "react-router-dom"
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../Components/Movie";
+
+const Container = styled.div`
+    height: 100vh;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    width:100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+
+`;
+const Column = styled.div`
+    margin-left: 10px;
+    width: 50%;
+`;
+const Title = styled.h1`
+    font-size: 65px;
+    margin-bottom: 15px;
+`;
+
+const SubTitle = styled.h3`
+    font-size: 35px;
+    margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+    font-size: 28px;
+`;
+
+const Poster = styled.div`
+    width: 25%;
+    height: 60%;
+    background-color: transparent;
+    background-image: url(${props => props.bg});
+    background-size: cover;
+    background-position: center center;
+`;
+
+const Suggestion = styled.div`
+`;
+
+
+const GET_MOVIE = gql`
+    query getMovie($id:Int!) {
+        movie(id: $id) {
+            id
+            title
+            year
+            rating
+            medium_cover_image
+            language
+            description_intro
+        }
+        suggestion(id: $id) {
+            id
+            title
+            medium_cover_image
+  	    }
+    }
+`;
+
+
+export default () => {
+    let {id} = useParams();
+    id=parseInt(id);
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id }
+    });
+    console.log("data:", data);
+    return (
+        <>
+        <Container>
+            <Column>
+                <Title>{loading ? "Loading..." : data.movie.title }</Title>
+                {!loading && data.movie && (
+                <>
+                    <SubTitle>
+                        {data.movie.language} · {data.movie.rating}
+                    </SubTitle>
+                    <Description>
+                        {data.movie.description_intro}
+                    </Description>
+                </>
+                
+                )}
+            </Column>
+            <Poster bg={data && data.movie ? data.movie.medium_cover_image : "" }></Poster>
+        </Container>
+        <Suggestion>
+            {data && data.suggestion && data.suggestion.map(movie => (<Movie id={movie.id} bg={movie.medium_cover_image}>{movie.title}</Movie>))}
+        </Suggestion>
+        </>
+    )
+    };
--- Documents/apollo-2020/src/routes/Home.js
+++ Documents/apollo-2020/src/routes/Home.js
@@ -1,6 +1,48 @@
 import React from "react";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
+import Movie from "Components/Movie";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display:flex;
+    flex-direction : column;
+    align-items: center;
+    width: 100%;
+`;
+const Header = styled.div`
+    background-image: linear-gradient(-45deg, #4ECDC4,#556270);
+    height: 45vh;
+    color:white;
+    width :100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+const Title = styled.h1`
+    font-size:60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`;
+const SubTitle = styled.div`
+    font-size: 35px;
+`;
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
+`;
+
 
 const GET_MOVIES = gql`
     {
@@ -13,12 +55,19 @@ const GET_MOVIES = gql`
 
 export default () => {
     const {loading, data} = useQuery(GET_MOVIES);
-    console.log(loading, data);
-    if(loading) {
-        return "loading..."
-    }
-    if(data && data.movies) {
-        return data.movies.map(movie => <h1>{movie.id}</h1>)
-    }
-    return <h1>Home</h1>
+    return (
+        <Container>
+            <Header>
+                <Title>2020</Title>
+                <SubTitle>I love GraphQL</SubTitle>
+            </Header>
+            {loading &&< Loading>Loading...</Loading>}
+            {!loading && data.movies && (
+                <Movies>
+                    {data.movies.map(movie =>
+                    (<Movie key={movie.id} id={movie.id} bg={movie.medium_cover_image} />))}
+                </Movies>
+            )}
+        </Container>
+    )
 }
