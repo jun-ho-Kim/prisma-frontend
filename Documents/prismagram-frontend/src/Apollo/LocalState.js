8ac9f48d348b11e8eb7cdc761f66f9eab807289e
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-07-06 14:32:19 +0900

    Apollo Client

---
+++ Documents/prismagram-frontend/src/Apollo/LocalState.js
@@ -0,0 +1,24 @@
+export const defaults = {
+    isLoggedIn: Boolean(localStorage.getItem("token")) || false
+};
+
+export const resolvers = {
+    // context 상의 token이나 cache 같은
+    // arguments를 가질 것 이다.
+    Mutatiaon: {
+        logUserIn: (_, {token}, {cache}) => {
+            localStorage.setItem("token", token);
+            cache.writeData({
+                data: {
+                    isLoggedIn: true
+                }
+            });
+            return null;
+        },
+        logUserOut: (_, __, {cache}) => {
+            localStorage.removeItem("token");
+            window.location.reload();
+            return null;
+        }
+    }
+};
