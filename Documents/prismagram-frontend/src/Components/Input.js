3d0137f966f6bb5d870e2fc0e72c6cf0faed50cb
todayIsHoho <42564119+jun-ho-Kim@users.noreply.github.com>
2020-07-07 14:15:39 +0900

    AuthForm with Hooks

--- Documents/prismagram-frontend/src/Components/Input.js
+++ Documents/prismagram-frontend/src/Components/Input.js
@@ -5,10 +5,27 @@ import styled from "styled-components";
 const Container = styled.input`
 `;
 
-const Input = ({placeholder}) => <Container placeholder={placeholder} />;
+const Input = ({
+    placeholder,
+    required = true,
+    value,
+    onChange,
+    type = "text"
+}) => 
+    <Container 
+        placeholder={placeholder}
+        required={required}
+        value={value}
+        onChange={onChange}
+        type={type}
+    />;
 
 Input.propTypes = {
-    placeholder: PropTypes.string.isRequired
+    placeholder: PropTypes.string.isRequired,
+    required: PropTypes.bool,
+    value: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired,
+    type: PropTypes.string
 };
 
 export default Input;
